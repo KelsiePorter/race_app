49902df2b6931f1c43b21f9114df3bab3616c0b6
Kelsie Porter <krporter22@gmail.com>
2022-11-30 20:43:23 -0700

    Create seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+race_1 = Race.create!(
+  name: "Turkey Trot",
+  location: "Salida",
+  professional_racers_only: false,
+  kilometers: 50,
+  date: DateTime.parse('20-05-2023')
+)
+race_2 = Race.create!(
+  name: "Tough Mudder",
+  location: "Moab",
+  professional_racers_only: true,
+  kilometers: 10,
+  date: DateTime.parse('05-11-2023')
+)
+
+participant_1 = Participant.create!(
+  first_name: "Kristen",
+  last_name: "Bell",
+  professional_racer: true,
+  age: 78,
+  race_id: race_2.id
+) 
+participant_2 = Participant.create!(
+  first_name: "Tom",
+  last_name: "Havorford",
+  professional_racer: false,
+  age: 40,
+  race_id: race_1.id
+) 
+participant_3 = Participant.create!(
+  first_name: "Bodi",
+  last_name: "Flaten",
+  professional_racer: true,
+  age: 8,
+  race_id: race_2.id
+) 
+participant_4 = Participant.create!(
+  first_name: "Dean",
+  last_name: "Kelly",
+  professional_racer: false,
+  age: 87,
+  race_id: race_1.id
+)
